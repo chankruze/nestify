626d2996da13b87555f34ab6996d5342614307cd
chankruze <chankruze@gmail.com>
2023-12-11 16:06:34 +0530

    initial nest + fastify setup
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true }),
+  );
+  await app.listen(3000, '0.0.0.0'); // specify '0.0.0.0' in the listen() for all hosts
+}
+bootstrap();
+
+// https://docs.nestjs.com/techniques/performance
+// https://fastify.dev/docs/latest/Reference/Middleware/
